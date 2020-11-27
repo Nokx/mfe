75919a61eca4b041f49891ca3e811beb2a61b14b
Sergei Novikov <noxlen.serg@gmail.com>
2020-11-27 18:15:03 +0500

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -17,5 +19,10 @@ module.exports = {
             }
         ]
 
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 }
